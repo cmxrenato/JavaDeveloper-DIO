e61168725a423ac6cb6a2f7f673099c883f6daf6
cmxrenato <renatoaraujoleal@gmail.com>
2024-11-30 19:35:36 -0300

    Operador ternário

---
+++ src/exem/aulas/OperadorTernario.java
@@ -0,0 +1,17 @@
+package exem.aulas;
+
+public class OperadorTernario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 6;
+		int b = 6;
+		
+		// Se a for igual a b então 1 senão 0.
+		int resultado = a == b ? 1 : 0;
+		
+		System.out.println(resultado);
+	}
+
+}
